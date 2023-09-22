eb811db42b73258b9f3bbce6ac4f109153b397b4
Rizwan <rizwan@local.com>
2023-09-22 20:43:01 +0530

    FIXED: changes

--- .platform/hooks/predeploy/01_npm.sh
+++ .platform/hooks/predeploy/01_npm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Clearing npm cache"
-npm cache --force clean
-rm -rf /var/app/current/package-lock.json
+# npm cache --force clean
+# rm -rf /var/app/current/package-lock.json
 # echo "npm install"
 # cd /var/app/current
 # npm install -f
